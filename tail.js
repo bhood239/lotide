45788f696b46b86d95cf3c0c65e3c6ba7a327ed3
Ben Hood <benhood97@btinternet.com>
2024-01-25 21:40:38 -0800

    added some extra tests

--- tail.js
+++ tail.js
@@ -11,8 +11,20 @@ const tail = function(arr) {
   return newArr;
 };
 
-// Test code
+// Test code 1
 const words = ['yes', 'no', 'maybe'];
 
 tail(words);
 assertEqual(words.length, 3);
+
+// Test code 2
+let newWords = tail(words);
+assertEqual(words.length, newWords.length);
+
+// Test code 3
+let test3 = ['one'];
+assertEqual(tail(test3), 1);
+
+// Test code 4
+let test4 = [];
+assertEqual(tail(test4), 1);
