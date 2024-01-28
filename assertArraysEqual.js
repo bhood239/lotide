b0c2f04c3b313ea62129c4fbacb3445eda86ca19
Ben Hood <benhood97@btinternet.com>
2024-01-27 17:13:38 -0800

    implemented the assaertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,17 @@
-const assertArraysEqual = function(actual, expected) {
-  let string1 = actual.toString();
-  let string2 = expected.toString();
-  if (string1 === string2) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+let eqArrays = function(arr1, arr2) {
+  let string1 = arr1.toString();
+  let string2 = arr2.toString();
+  return string1 === string2;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if(eqArrays(arr1, arr2) == true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 4, 3], [1, 2, 3]);
+
