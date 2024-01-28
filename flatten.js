acdce6a8eb7a28b07c705059662954d31a9e0ac1
Ben Hood <benhood97@btinternet.com>
2024-01-28 11:07:33 -0800

    made eqArrays strict equality

--- flatten.js
+++ flatten.js
@@ -1,9 +1,9 @@
 let eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   } else 
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] != arr2[i]) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       };
       }
