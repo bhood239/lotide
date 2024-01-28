3af4831c7a6f51dcbc90d8df0134dac879072bb8
Ben Hood <benhood97@btinternet.com>
2024-01-28 09:57:37 -0800

    added improved eqArrays function

--- without.js
+++ without.js
@@ -1,8 +1,14 @@
 let eqArrays = function(arr1, arr2) {
-  let string1 = arr1.toString();
-  let string2 = arr2.toString();
-  return string1 === string2;
-};
+  if (arr1.length != arr2.length) {
+    return false;
+  } else 
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        return false;
+      };
+      }
+      return true
+    };
 
 const assertArraysEqual = function(arr1, arr2) {
   if(eqArrays(arr1, arr2)) {
