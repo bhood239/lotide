b5b4c04f0676ba02cfedeb3b9ff1f09ceb9db36d
Ben Hood <benhood97@btinternet.com>
2024-01-30 13:30:19 -0800

    got rid of unneccesary spaces

--- countOnly.js
+++ countOnly.js
@@ -8,7 +8,6 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function (allItems, itemsToCount) {
   const results = {}
-
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
@@ -20,7 +19,6 @@ const countOnly = function (allItems, itemsToCount) {
   }
   return results;
 }
-
 
 const firstNames = [
   "Karl",
