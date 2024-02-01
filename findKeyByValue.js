b9870b805301d8604ccbaafe8360e3746d069af4
Ben Hood <benhood97@btinternet.com>
2024-01-31 19:05:53 -0800

    added function

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(testObject, valueToFind) {
+  let result = undefined;
+
+  let keys = Object.keys(testObject);
+  let values = Object.values(testObject);
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === valueToFind) {
+      result = keys[i];
+      break;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "sci_fi"); //set to fail
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
