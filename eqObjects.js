186817b4fedbbcb86aab3fd863931b775e05ecb3
Ben Hood <benhood97@btinternet.com>
2024-02-03 12:15:38 -0800

    added function to compare if 2 objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else 
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      };
+      }
+      return true
+    };
+
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  
+  if(keys1.length !== keys2.length) {
+    return false;
+  } else 
+    for (const key of keys2) {
+      
+      if(Array.isArray(Object.values(object1))) {
+        eqArrays(object1[key], object2[key]);
+      }
+      
+      else if(object1[key] !== object2[key]) {
+        return false;
+      }
+    } return true;
+
+}
+
+const suitObject = {color: "blue", size: "large", piece: "3"};
+const anotherSuitObject = {size: "large", piece: "3", color: "blue"};
+//assertEqual(eqObjects(suitObject, anotherSuitObject), true);
+
+const dressObject = {color: "red", size: "large", design: "flowers"};
+//assertEqual(eqObjects(suitObject, dressObject), false);
+
+//assertEqual(eqObjects(dressObject, anotherSuitObject), false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
