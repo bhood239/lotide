6843d9a76b2cdd127c498c1aeec47082b1405c6f
Ben Hood <benhood97@btinternet.com>
2024-02-03 12:51:08 -0800

    changed eqObjects to check if both values are arrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -18,7 +18,7 @@ const eqObjects = function(object1, object2) {
     return false;
   } else 
     for (const key of keys2) {
-      if(Array.isArray(object1[key])) {
+      if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if(!eqArrays(object1[key], object2[key])) {
           return false;
         }
