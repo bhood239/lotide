c476e6a79b795e9262741479b53fd83b141e6e11
Ben Hood <benhood97@btinternet.com>
2024-02-03 18:46:55 -0800

    added takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else 
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      };
+      }
+      return true
+    };
+
+const assertArraysEqual = function(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  result = [];
+  for (const item of array) {
+    if (callback(item) === false) {
+      result.push(item);
+    } else break;
+  }
+  return result;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
