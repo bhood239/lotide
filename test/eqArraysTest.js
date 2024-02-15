bd8c2eef11757d9ebd168f3bdede6c713aaacd76
Ben Hood <benhood97@btinternet.com>
2024-02-14 18:54:29 -0800

    refactored assertArraysEqual

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,4 +4,6 @@ const eqArrays = require(`../eqArrays`);
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
-assertEqual(eqArrays([3, 2, 1], [3, 2, 1]), true);
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); //fail test
